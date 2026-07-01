045099d01a4bd90e3021c3183cb2d4a06b8db2b9
Ahamed Sumon <ahamedsumon13579@gmail.com>
2026-07-01 16:49:31 +0300

    added login and register form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,42 @@
+import { Link } from "react-router";
+
 const Login = () => {
   return (
-    <div>
-      <h3>Login Page</h3>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+      <div className="card-body">
+        <h1 className="text-4xl font-bold">Please Login!</h1>
+        <form>
+          <fieldset className="fieldset">
+            {/* Email Field */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+            />
+            {/* Password Field */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+            />
+
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+        <p>
+          New to our website? Please{" "}
+          <Link
+            className="text-blue-500 hover:text-blue-600 hover:underline"
+            to="/register"
+          >
+            register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,57 @@
+import { Link } from "react-router";
+
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, email, password);
+  };
   return (
-    <div>
-      <h3>Register Page</h3>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+      <div className="card-body">
+        <h1 className="text-4xl font-bold">Please Register!</h1>
+        <form onSubmit={handleRegister}>
+          <fieldset className="fieldset">
+            {/* Name Field */}
+            <label className="label">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Name"
+            />
+            {/* Email Field */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+            />
+            {/* Password Field */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+            />
+
+            <button className="btn btn-neutral mt-4">Register</button>
+          </fieldset>
+        </form>
+        <p>
+          Already have an account? Please{" "}
+          <Link
+            className="text-blue-500 hover:text-blue-600 hover:underline"
+            to="/login"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
